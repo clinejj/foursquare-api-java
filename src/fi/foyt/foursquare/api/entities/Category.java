65393a174d8c5992c1332ac1ebfb8f6f476b205c
antti.leppa@foyt.fi <antti.leppa@foyt.fi@3510f92a-767f-308a-6afb-0984ff0fccdf>
2011-05-28 09:49:46 +0000

    ** Added pluralName property to Category entity
    git-svn-id: http://foursquare-api-java.googlecode.com/svn/foursquare-api@69 3510f92a-767f-308a-6afb-0984ff0fccdf

--- src/fi/foyt/foursquare/api/entities/Category.java
+++ src/fi/foyt/foursquare/api/entities/Category.java
@@ -24,6 +24,10 @@ public class Category implements FoursquareEntity {
   public String getName() {
     return name;
   }
+  
+  public String getPluralName() {
+    return pluralName;
+  }
 
   public String getIcon() {
     return icon;
@@ -43,6 +47,7 @@ public class Category implements FoursquareEntity {
 
   private String id;
   private String name;
+  private String pluralName;
   private String icon;
   private String[] parents;
   private Boolean primary;
